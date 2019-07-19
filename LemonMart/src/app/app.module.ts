b9b7dfeae07d231ae12bc1a3066f89c3c31285fa
Farooq Mahmud <farooq.mahmud@marel.com>
2019-07-19 14:37:38 -0700

    Added frature modules

--- LemonMart/src/app/app.module.ts
+++ LemonMart/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
+import { ManagerModule } from './manager/manager.module';
+import { InventoryModule } from './inventory/inventory.module';
+import { PosModule } from './pos/pos.module';
+import { UserModule } from './user/user.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,11 @@ import { MaterialModule } from './material.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MaterialModule
+    MaterialModule,
+    ManagerModule,
+    InventoryModule,
+    PosModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
