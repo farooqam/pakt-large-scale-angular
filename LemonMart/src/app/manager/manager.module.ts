c994dbf5f8cc6ec9e541edf07c9d1dc5f8d99d75
Farooq Mahmud <farooq.mahmud@marel.com>
2019-07-20 11:12:52 -0700

    Manager component

--- LemonMart/src/app/manager/manager.module.ts
+++ LemonMart/src/app/manager/manager.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { ManagerRoutingModule } from './manager-routing.module';
 import { ManagerHomeComponent } from './manager-home/manager-home.component';
+import { ManagerComponent } from './manager.component';
+import { MaterialModule } from '../material.module';
 
 
 @NgModule({
-  declarations: [ManagerHomeComponent],
+  declarations: [ManagerHomeComponent, ManagerComponent],
   imports: [
     CommonModule,
+    MaterialModule,
     ManagerRoutingModule
   ]
 })
