218c827c389a7ae159fce2b172133a6edaae9624
Farooq Mahmud <farooq.mahmud@marel.com>
2019-07-21 20:08:46 -0700

    Inventory home component

--- LemonMart/src/app/inventory/inventory-routing.module.ts
+++ LemonMart/src/app/inventory/inventory-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InventoryComponent } from './inventory.component';
+import { InventoryHomeComponent } from './inventory-home/inventory-home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: InventoryComponent, children: [
+      { path: '', redirectTo: '/inventory/home', pathMatch: 'full' },
+      { path: 'home', component: InventoryHomeComponent }
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- LemonMart/src/app/inventory/inventory.module.ts
+++ LemonMart/src/app/inventory/inventory.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { InventoryRoutingModule } from './inventory-routing.module';
+import { InventoryComponent } from './inventory.component';
+import { MaterialModule } from '../material.module';
+import { InventoryHomeComponent } from './inventory-home/inventory-home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [InventoryComponent, InventoryHomeComponent],
   imports: [
     CommonModule,
+    MaterialModule,
     InventoryRoutingModule
   ]
 })
