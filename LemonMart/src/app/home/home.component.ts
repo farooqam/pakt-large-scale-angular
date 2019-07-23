9004835b44f16d87f3b46f2feffaf2b02ab3c706
Farooq Mahmud <farooq.mahmud@marel.com>
2019-07-23 14:06:28 -0700

    login

--- LemonMart/src/app/home/home.component.ts
+++ LemonMart/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  displayLogin = true;
-  constructor() { }
+  // tslint:disable-next-line:variable-name
+  private _displayLogin = true;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authService.authStatus.subscribe(authStatus => this._displayLogin = !authStatus.isAuthenticated);
+  }
+
+  get displayLogin() {
+    return this._displayLogin;
   }
 
 }
